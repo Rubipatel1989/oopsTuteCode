cd57d65587ef732368e8251e7fe9bfe5b700bc9b
Pawan Kumar <pawankum0010@gmail.com>
2024-04-24 18:04:25 +0530

    Add New

---
+++ mysql/query_rowQuery_difference.sql
@@ -0,0 +1,17 @@
+"row query" => typically refers to a query that retrieves one or more rows from a database table. 
+    This is the most common type of query and is used to select, insert, update, or delete rows.
+
+"query"  => in general can refer to any SQL statement that retrieves, modifies, 
+    or manages data in a database. This includes not only row queries but also other 
+    types of queries like aggregate queries (e.g., COUNT, SUM, AVG), subqueries, 
+    and administrative queries (e.g., CREATE, ALTER, DROP).
+
+So, while a "row query" is a specific type of query that deals with rows of data, 
+the term "query" can encompass a wider range of SQL statements that interact 
+with a database.
+
+
+
+
+
+
